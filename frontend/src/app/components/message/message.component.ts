94c4d482a1d055e5423719be5e3133eeab523745
Carlo Rosso <carlorosso@Carlos-MacBook-Air.local>
2024-05-15 18:53:24 +0200

    switch di update e notify, perché avevo sbagliato ad usare i termini

--- frontend/src/app/components/message/message.component.ts
+++ frontend/src/app/components/message/message.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { MessageService } from '../../services/lib/message.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-message',
@@ -12,10 +11,9 @@ import { Subscription } from 'rxjs';
 export class MessageComponent {
   messages: { message: string; type: 'log' | 'error' }[];
   message_service = inject(MessageService);
-  subscription: Subscription;
 
   ngOnInit(): void {
-    this.subscription = this.message_service.notify().subscribe((messages) => {
+    this.message_service.notify().subscribe((messages) => {
       this.messages = messages;
     });
   }
