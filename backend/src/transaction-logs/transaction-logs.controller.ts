33155e6d9924ddf868219578626a3d54a205d47d
Matteo Bando <bandomatteo@gmail.com>
2024-05-17 20:58:09 +0200

    chore: Add Swagger annotations and decorators for transaction-logs controller endpoints

--- backend/src/transaction-logs/transaction-logs.controller.ts
+++ backend/src/transaction-logs/transaction-logs.controller.ts
@@ -4,6 +4,8 @@ import { CreateTransactionLogDto } from './dto/create-transaction-log.dto';
 import { UpdateTransactionLogDto } from './dto/update-transaction-log.dto';
 import { AuthorizationService } from '../authorization/authorization.service';
 import { ResultTransactionLogDto } from './dto/result-transaction-log.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+
 
 @Controller('transaction-logs')
 export class TransactionLogsController {
@@ -14,6 +16,9 @@ export class TransactionLogsController {
   ) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Crea un nuovo log di transazione'})
+  @ApiResponse({ status: 200, description: 'Log di transazione creato con successo.', type: ResultTransactionLogDto })
+  @ApiBody({ type: CreateTransactionLogDto })
   async create(@Body() createTransactionLogDto: CreateTransactionLogDto, @Req() req): Promise<ResultTransactionLogDto> {
     const accessToken = req.cookies.accessToken;
     const auth = this.authorizationService.isAuthorized(accessToken);
@@ -21,6 +26,9 @@ export class TransactionLogsController {
   }
 
   @Get('find-all-by-bill/:id')
+  @ApiOperation({ summary: 'Cerca tutti i log di transazione associati ad un ID fattura'})
+  @ApiResponse({ status: 200, description: 'Log di transazione trovati con successo.', type: ResultTransactionLogDto })
+  @ApiParam({ name: 'id', type: 'number', description: 'ID fattura' })
   async findAllByBillId(@Param('id') id: string, @Req() req): Promise<ResultTransactionLogDto> {
     const accessToken = req.cookies.accessToken;
     const auth = this.authorizationService.isAuthorized(accessToken);
@@ -29,6 +37,9 @@ export class TransactionLogsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Rimuovi un log di transazione specifico basandosi sul suo ID'})
+  @ApiResponse({ status: 200, description: 'Log di transazione rimosso con successo.', type: ResultTransactionLogDto })
+  @ApiParam({ name: 'id', type: 'number', description: 'ID log di transazione' })
   async remove(@Param('id') id: string, @Req() req): Promise<ResultTransactionLogDto> {
     const accessToken = req.cookies.accessToken;
     const auth = this.authorizationService.isAuthorized(accessToken);
