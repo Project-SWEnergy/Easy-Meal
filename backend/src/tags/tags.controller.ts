fe603d5d52e481a8d60a4d164e32c059b90b77b6
Matteo Bando <bandomatteo@gmail.com>
2024-05-17 20:59:50 +0200

    chore: Add Swagger annotations and decorators for tags controller endpoints

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -3,6 +3,7 @@ import { TagsService } from './tags.service';
 //import { AuthorizationService } from 'src/authorization/authorization.service';
 import { ResultTagDto } from './dto/result-tag.dto';
 //import { UpdateTagDto } from './dto/update-tag.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('tags')
 export class TagsController {
@@ -13,17 +14,25 @@ export class TagsController {
   ) { }
 
   @Get('find-all')
+  @ApiOperation({ summary: 'Cerca tutti i tag' })
+  @ApiResponse({ status: 200, description: 'Tag trovati con successo.', type: ResultTagDto })
   async findAll(): Promise<ResultTagDto> {
     return await this.tagsService.findAll();
   }
 
   @Get('find-one/:id')
+  @ApiOperation({ summary: 'Cerca un tag specifico basandosi sul suo ID' })
+  @ApiResponse({ status: 200, description: 'Tag trovato con successo.', type: ResultTagDto })
+  @ApiParam({ name: 'id', type: 'number', description: 'ID tag' })
   async findOne(@Param('id') id: string): Promise<ResultTagDto> {
     const tagId = parseInt(id);
     return await this.tagsService.findOne(tagId);
   }
 
   @Get('find-by-name/:name')
+  @ApiOperation({ summary: 'Cerca un tag specifico basandosi sul suo nome' })
+  @ApiResponse({ status: 200, description: 'Tag trovato con successo.', type: ResultTagDto })
+  @ApiParam({ name: 'name', type: 'string', description: 'Nome tag' })
   async findByName(@Param('name') name: string): Promise<ResultTagDto> {
     return await this.tagsService.findByName(name);
   }
